3253768884ebe24a505109345dba59bc174c4fc1
Никита Кем <kem.nik26@gmail.com>
2024-12-25 16:57:40 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect
 from db import db
 from db.models import users, articles
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, logout_user, current_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -63,3 +63,10 @@ def login():
 @login_required
 def article_list():
     return 'Список статей'
+
+
+@lab8.route('/lab8/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8')
